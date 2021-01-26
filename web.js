72bbd8cf18d6dd7975f9d7fa3d1e9440bf56a3c8
Marcelinaaaa <marcelina808@poczta.onet.pl>
2021-01-27 00:29:19 +0100

    GET, POST and GET by id items from shopping list

--- web.js
+++ web.js
@@ -2,16 +2,34 @@ var PORT = process.env.PORT || 3000;
 var express = require("express");
 var app = express();
 
+var uniqueId= 4;
+var items = [{id:1,name:'bread', price: '3,50'}, {id:2,name:'butter', price: '4,50'}, {id:3,name:'milk', price: '2,50'}];
+
+app.use(express.urlencoded());
+
 app.get("/", function(req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Shopping list!');
 });
+app.get('/items', (req, res) => {
+	var shoppingList = [];
+	items.forEach(item => shoppingList.push(item.name));
+  res.json(shoppingList);
+});
+app.post('/items', (req, res) => {    
+	const newItem = {...req.body, id:uniqueId++};
+	items.push(newItem);
+	res.sendStatus(201);
+});
+app.get('/items/:id', (req, res) => {
+	const id = req.params.id;
+	const itemFoundById = items.find(x => x.id == id);
+	var text = "Product name: "+itemFoundById.name+", price: "+itemFoundById.price+".";
+	//res.end(itemFoundById);
+	res.end(text);
+});
+
 app.listen(PORT);
-
-
-
-
-
 
 
 
